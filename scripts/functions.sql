67128ff685c402df726726390a421bb1987c8130
timermakov <ts.ermakov@yandex.ru>
2023-12-06 12:38:35 +0300

    Added functions

--- scripts/functions.sql
+++ scripts/functions.sql
@@ -20,7 +20,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT * FROM get_units_by_race('Terran');
+-- SELECT * FROM get_units_by_race('Terran');
 
 
 -- Функция для получения всех building по выбранной race
@@ -42,7 +42,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT * FROM get_buildings_by_race('Protoss');
+-- SELECT * FROM get_buildings_by_race('Protoss');
 
 -- Функция для получения всех hero по выбранной race
 CREATE OR REPLACE FUNCTION get_heroes_by_race(race_name_param text)
@@ -61,5 +61,46 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT * FROM get_heroes_by_race('Zerg');
+-- SELECT * FROM get_heroes_by_race('Zerg');
 
+
+-- Хранимая процедура для обновления здоровья юнита на основе полученного урона
+CREATE OR REPLACE PROCEDURE update_unit_health(target_unit_id INT, damage_value INT)
+    LANGUAGE plpgsql AS $$
+BEGIN
+    UPDATE unit
+    SET health_id = health_id - damage_value
+    WHERE unit.unit_id = target_unit_id;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Функция для создания ресурсного участка на карте
+CREATE OR REPLACE FUNCTION create_resource_patch(target_map_id INT, target_resource_type_id INT, target_quantity INT, target_x INT, target_y INT)
+    RETURNS VOID AS $$
+BEGIN
+    INSERT INTO resource_patch (map_id, resource_type_id, quantity, x, y)
+    VALUES (target_map_id, target_resource_type_id, target_quantity, target_x, target_y);
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Хранимая процедура для создания препятствия на карте
+CREATE OR REPLACE PROCEDURE create_obstacle(target_obstacle_name TEXT, target_x INT, target_y INT, target_map_id INT)
+    LANGUAGE plpgsql AS $$
+BEGIN
+    INSERT INTO obstacle (obstacle_name, x, y, map_id)
+    VALUES (target_obstacle_name, target_x, target_y, target_map_id);
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+-- Функция для создания монолога героя
+CREATE OR REPLACE FUNCTION create_hero_monologue(target_content TEXT, target_hero_id INT)
+    RETURNS VOID AS $$
+BEGIN
+    INSERT INTO hero_monologue (content, hero_id)
+    VALUES (target_content, target_hero_id);
+END;
+$$ LANGUAGE plpgsql;
