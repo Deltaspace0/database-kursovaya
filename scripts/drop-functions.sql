a75903929c906ea863be457825736050e2fa4958
Ruslan Gadeev <rgadeew@yandex.ru>
2023-12-06 22:33:03 +0300

    add script to drop functions

---
+++ scripts/drop-functions.sql
@@ -0,0 +1,7 @@
+drop function get_units_by_race;
+drop function get_buildings_by_race;
+drop function get_heroes_by_race;
+drop function create_resource_patch;
+drop function create_hero_monologue;
+drop procedure update_unit_health;
+drop procedure create_obstacle;
